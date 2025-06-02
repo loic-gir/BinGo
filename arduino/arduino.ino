972936ecc2effec05e054b9ee7eba428ff6599e8
Xingtong <xingtonglin011016@gmail.com>
2025-06-02 15:49:38 +0200

    ajt affichageCSV pr site

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -2,6 +2,10 @@
 // #include "HX711.h"
 #define NB_bac 5
 
+// Configuration des distances
+#define HAUTEUR_BAC_CM 100  // hauteur totale d’un bac => 0% plein
+#define DISTANCE_MIN_CM 10  // distance minimale => 100% plein
+
 // // Définition des pins de l’Arduino pour le capteur de poids
 // #define DT 2 // DT = Data (signal de données)
 // #define SCK 3 // SCK = Serial Clock (signal d’horloge)
@@ -13,6 +17,8 @@ const int echoPins[NB_bac] = {7,8,9,10,11}; // Idem pour les echo
 
 // Variable pour savoir si un sac est plein
 bool estPlein[NB_bac] = {false, false, false, false, false}; 
+
+float pourcentages[NB_bac];  // Pour stocker les % de remplissage
 
 // // Création des objets Servo
 Servo servo1; // FS5106B
@@ -39,6 +45,52 @@ void bougerServo(Servo& monServo, int angleCible) {
     }
   }
   delay(300); // Pause à destination
+}
+
+float calculerPourcentage(float distance) {
+  if (distance <= DISTANCE_MIN_CM) return 100.0;
+  if (distance >= HAUTEUR_BAC_CM) return 0.0;
+  return ((HAUTEUR_BAC_CM - distance) / (HAUTEUR_BAC_CM - DISTANCE_MIN_CM)) * 100.0;
+}
+
+float calculDistance(int numBac) { // de 0 a 4
+  // Envoie du signal de déclenchement
+  digitalWrite(trigPins[numBac], LOW);   // 1. On s'assure que le trigger est à 0V
+  delayMicroseconds(2);                  // 2. Petit délai
+  digitalWrite(trigPins[numBac], HIGH);  // 3. On envoie une impulsion (5V)
+  delayMicroseconds(10);                  // 4. Pendant 10 microsecondes
+  digitalWrite(trigPins[numBac], LOW);   // 5. On coupe l’impulsion
+
+  // Réception du signal
+  // mesure combien de temps (en microsecondes) le pin echoPin reste à l’état HIGH (5V)
+  long duration = pulseIn(echoPins[numBac], HIGH);
+  // Le son voyage à ~340 m/s, soit 0.034 cm/µs
+  float distance = duration * 0.034 / 2;
+
+  // a supprimer apres test
+  Serial.print("Capteur ");
+  Serial.print(numBac + 1); // de 1 a 5
+  Serial.print(" : ");
+  Serial.print(distance);
+  Serial.println(" cm");
+
+  // Si distance < 10 cm 
+  if (distance < 10) {
+    estPlein[numBac] = true;
+  }
+  else estPlein[numBac] = false;
+
+  return distance;
+}
+
+void affichageCSV(){
+  // Affichage sous forme CSV
+  Serial.print("forSite"); // Ajout du mot fixe
+  for (int i = 0; i < NB_bac; i++) {
+    Serial.print(pourcentages[i], 0); // 0 chiffre après virgule
+    if (i < NB_bac - 1) Serial.print(",");
+  }
+  Serial.println();
 }
 
 void setup() {
@@ -71,33 +123,14 @@ void setup() {
   // bougerServo(servo2,90);
   // bougerServo(servo3,90);
   // bougerServo(servo4,90);
-}
 
-void calculDistance(int numBac) { // de 0 a 4
-  // Envoie du signal de déclenchement
-  digitalWrite(trigPins[numBac], LOW);   // 1. On s'assure que le trigger est à 0V
-  delayMicroseconds(2);                  // 2. Petit délai
-  digitalWrite(trigPins[numBac], HIGH);  // 3. On envoie une impulsion (5V)
-  delayMicroseconds(10);                  // 4. Pendant 10 microsecondes
-  digitalWrite(trigPins[numBac], LOW);   // 5. On coupe l’impulsion
-
-  // Réception du signal
-  // mesure combien de temps (en microsecondes) le pin echoPin reste à l’état HIGH (5V)
-  long duration = pulseIn(echoPins[numBac], HIGH);
-  // Le son voyage à ~340 m/s, soit 0.034 cm/µs
-  float distance = duration * 0.034 / 2;
-
-  Serial.print("Capteur ");
-  Serial.print(numBac + 1); // de 1 a 5
-  Serial.print(" : ");
-  Serial.print(distance);
-  Serial.println(" cm");
-
-  // Si distance < 10 cm 
-  if (distance < 10) {
-    estPlein[numBac] = true;
+  for (int i = 0; i < NB_bac; i++) {
+    float distance = calculDistance(i);
+    pourcentages[i] = calculerPourcentage(distance);
+    delay(50);
   }
-  else estPlein[numBac] = false;
+  affichageCSV();
+
 }
 
 void loop() {
@@ -110,7 +143,7 @@ void loop() {
   // Contrôle des servomoteurs en fonction des mesures (type dechet)
   int valeur = random(0, NB_bac); // 5 est exclu, donc ça donne 0 à 4 inclus
 
-  // if (!estPlein[valeur] && poids < 0.5) { // 25 cm, 0.5 kg
+  // if (!estPlein[valeur] && poids < 0.5) { // 0.5 kg
   if (!estPlein[valeur]) { // test sans poids
     Serial.print("Déplacement vers bac ");
     Serial.println(valeur);
@@ -121,8 +154,10 @@ void loop() {
     // bougerServo(servo3, angleS3[valeur]);
     // bougerServo(servo4, angleS4[valeur]);
 
-    calculDistance(valeur);
-
+    float nvDistance = calculDistance(valeur);
+    pourcentages[valeur] = calculerPourcentage(nvDistance);
+    affichageCSV(); // pour affichage site
+    
   } else {
     Serial.println("Ce bac est plein, on ne déplace pas.");
   }
