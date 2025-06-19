f4378072b1bbdaeac631e8c0c7b1bbbd4aade391
Xingtong <xingtonglin011016@gmail.com>
2025-06-19 09:54:04 +0200

    fix bug style

--- site/js/scriptHome.js
+++ site/js/scriptHome.js
@@ -5,29 +5,31 @@ function initAnim() {
         // Configuration globale
         easing: 'ease-in-out',
         reset: true,
-        opcity: 0.5,
-        scale: 0.2
+        opcity: 0.5
     });
 
     // Animation pour les membres à droite
     ScrollReveal().reveal('.pers', {
         origin: 'top',
         distance: '2rem',
+        scale: 0.2
     });
 
     // Animation pour les paroles à gauche
     ScrollReveal().reveal('.parole-left', {
         origin: 'left',
-        distance: '50rem',
+        distance: '10rem',
         duration: 1600,
+        scale: 0.1,
         delay: 500
     });
 
     // Animation pour les paroles à droite
     ScrollReveal().reveal('.parole-right', {
         origin: 'right',
-        distance: '50rem',
+        distance: '10rem',
         duration: 1600,
+        scale: 0.1,
         delay: 500
     });
 
