6586a46760e545609745297e38b392928e2bb7a4
Xingtong <xingtonglin011016@gmail.com>
2025-06-26 08:51:28 +0200

    Update scriptSuivi.js

--- site/js/scriptSuivi.js
+++ site/js/scriptSuivi.js
@@ -1,5 +1,5 @@
 // === CONFIGURATION ===
-const NGROK_API_URL = "https://af6a-79-174-206-181.ngrok-free.app"; // URL de l'API Flask via ngrok
+const NGROK_API_URL = "https://4efa-79-174-206-181.ngrok-free.app"; // URL de l'API Flask via ngrok
 
 const NGROK_HEADERS = {
     'ngrok-skip-browser-warning': 'true',
